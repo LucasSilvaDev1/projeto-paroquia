528167a0da07fd9f9c815085df4e638ff390157e
Lucas silva <lucasdasilvaoliveira320@gmail.com>
2026-07-06 15:12:19 -0300

    site ajustado

--- public/js/liturgia.js
+++ public/js/liturgia.js
@@ -5,13 +5,20 @@ async function carregarLiturgia() {
 
   document.getElementById("titulo-do-dia").innerHTML = dados.titulodia;
   document.getElementById("versiculo-1l").innerHTML = dados.versiculoPl;
-  document.getElementById("primeiraLeitura").innerHTML += dados.primeiraLeitura;
+  document.getElementById("primeiraLeitura").innerHTML +=
+    dados.primeiraLeitura.slice(0, -199);
   document.getElementById("versiculo-sl").innerHTML = dados.versiculoSm;
-  document.getElementById("salmo").innerHTML += dados.salmo;
+  document.getElementById("salmo").innerHTML += dados.salmo.slice(0, -199);
+
+  const segundaleitura = document.getElementById("segundaleitura");
+  if (dados.segundaLeitura.length === 199) {
+    segundaleitura.style.display = "none";
+  }
   document.getElementById("versiculo-2l").innerHTML = dados.versiculoSl;
-  document.getElementById("segundaleitura").innerHTML += dados.segundaLeitura;
+  document.getElementById("segundaleitura").innerHTML += dados.segundaLeitura.slice(0, -199,
+  );
   document.getElementById("versiculo-E").innerHTML = dados.versiculoE;
-  document.getElementById("evangelho").innerHTML += dados.evangelho;
+  document.getElementById("evangelho").innerHTML += dados.evangelho.slice(0, -199);
 }
 
 carregarLiturgia();
