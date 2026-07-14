46b94149f3c9198d52e790c4515b468bdd5bf4ae
Lucas silva <lucasdasilvaoliveira320@gmail.com>
2026-07-14 14:37:48 -0300

    algumas modificações

---
+++ public/js/animações.js
@@ -0,0 +1,49 @@
+
+// const divAlvo1 = document.querySelector(".conteudo-sa");
+// const divAlvo2 = document.querySelector(".conteudo-h-p");
+// const divAlvo3 = document.querySelector(".conteudo-cap");
+
+// animar(divAlvo1)
+
+// function animar() {
+//   window.addEventListener('scroll', function animar() {
+//   var elemento = animar()
+//   var posicaoDoTopoDoElemento = elemento.getBoundingClientRect().top;
+//   var alturaDaTela = window.innerHeight;
+
+//   if (posicaoDoTopoDoElemento < alturaDaTela) {
+//     elemento.classList.add('animar1')
+//   }
+// })
+// window.addEventListener('scroll', function () {
+//   var elemento = document.querySelector('.conteudo-h-p')
+//   var posicaoDoTopoDoElemento = elemento.getBoundingClientRect().top;
+//   var alturaDaTela = window.innerHeight;
+
+//   if (posicaoDoTopoDoElemento < alturaDaTela) {
+//     elemento.classList.add('animar1')
+//   }
+// })
+// }
+
+
+
+
+// // window.addEventListener('scroll', function animar() {
+// //   var elemento = document.querySelector('.conteudo-h-p')
+// //   var posicaoDoTopoDoElemento = elemento.getBoundingClientRect().top;
+// //   var alturaDaTela = window.innerHeight;
+
+// //   if (posicaoDoTopoDoElemento < alturaDaTela) {
+// //     elemento.classList.add('animar1')
+// //   }
+// // })
+// // window.addEventListener('scroll', function () {
+// //   var elemento = document.querySelector('.conteudo-h-p')
+// //   var posicaoDoTopoDoElemento = elemento.getBoundingClientRect().top;
+// //   var alturaDaTela = window.innerHeight;
+
+// //   if (posicaoDoTopoDoElemento < alturaDaTela) {
+// //     elemento.classList.add('animar1')
+// //   }
+// // })
