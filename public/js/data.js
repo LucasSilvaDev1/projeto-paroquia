0088ab099f306b469f919883e89585b53dd77020
Lucas silva <lucasdasilvaoliveira320@gmail.com>
2026-07-20 16:21:23 -0300

    ajuste no conteúdo da pagina liturgia e no js.liiturgia

--- public/js/data.js
+++ public/js/data.js
@@ -1,11 +1,20 @@
 function obterDataAtual() {
   const hoje = new Date();
+  const ano = String(hoje.getFullYear());
+  const mes = String(hoje.getMonth());
+  const dia = String(hoje.getDate());
+  const diaSemana = hoje.getDay().toString()
+  const nomeMes = ["JAN","FEV","MAR","ABR","MAY","JUN","JUL","AGO","SET","OUT","NOV","DEZ"]
+  const nomeDia = ["Segunda-feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sabado", "Domingo" ]
+ 
+  const Dia = document.getElementById('dia')
+  const Mes = document.getElementById('mes')
+  const Ano = document.getElementById('ano')
+  const NomeSemana = document.getElementById('nome-dia')
 
-  const ano = hoje.getFullYear();
-
-  const mes = String(hoje.getMonth() + 1).padStart(2, "0");
-
-  const dia = String(hoje.getDate()).padStart(2, "0");
-
-  return `${ano}${mes}${dia}`;
+  Dia.innerText = dia
+  Mes.innerText = nomeMes[mes]
+  Ano.innerText = ano
+  NomeSemana.innerText = nomeDia[diaSemana-1]
 }
+obterDataAtual()
--- public/js/liturgia.js
+++ public/js/liturgia.js
@@ -6,19 +6,27 @@ async function carregarLiturgia() {
   document.getElementById("titulo-do-dia").innerHTML = dados.titulodia;
   document.getElementById("versiculo-1l").innerHTML = dados.versiculoPl;
   document.getElementById("primeiraLeitura").innerHTML +=
-    dados.primeiraLeitura.slice(0, -199);
+    dados.primeiraLeitura.slice(0, -199) + `
+    <p>- Palavra do Senhor.</p><p>- Graças a Deus.</p>`;
   document.getElementById("versiculo-sl").innerHTML = dados.versiculoSm;
   document.getElementById("salmo").innerHTML += dados.salmo.slice(0, -199);
 
   const segundaleitura = document.getElementById("segundaleitura");
   if (dados.segundaLeitura.length === 199) {
     segundaleitura.style.display = "none";
+  } else {
+    document.getElementById("versiculo-2l").innerHTML = dados.versiculoSl;
+    document.getElementById("segundaleitura").innerHTML +=
+      dados.segundaLeitura.slice(0, -199) +
+      `
+    <p>- Palavra do Senhor.</p><p>- Graças a Deus.</p>`;
   }
-  document.getElementById("versiculo-2l").innerHTML = dados.versiculoSl;
-  document.getElementById("segundaleitura").innerHTML += dados.segundaLeitura.slice(0, -199,
-  );
+
   document.getElementById("versiculo-E").innerHTML = dados.versiculoE;
-  document.getElementById("evangelho").innerHTML += dados.evangelho.slice(0, -199);
+  document.getElementById("evangelho").innerHTML +=
+    dados.evangelho.slice(0, -199) +
+    `
+    <p>- Palavra da Salvação.</p><p>- Gloria a vós, Senhor.</p>`;
 }
 
 carregarLiturgia();
